d89134de275f567bd993920565b1250597fc0598
ajit283 <55892788+ajit283@users.noreply.github.com>
2025-03-04 17:57:49 +0100

    .

---
+++ bash.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+
+# fzf
+apt install fzf
+source /usr/share/doc/fzf/examples/key-bindings.bash
+
+
+
+# gh
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
+
+gh auth login
+
+
+# nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+
+echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin' >> ~/.bashrc
+
+
+# rust
+curl https://sh.rustup.rs -sSf | sh
+
+# zellij
+cargo install --locked zellij
+
+# C-x C-e
+echo 'export $EDITOR=nvim' >> ~/.bashrc
